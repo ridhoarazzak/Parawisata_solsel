fb1358e883f0236ee48d5ecca8d17926d0bcfb02
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-07-14 20:03:02 +0700

    Update layout.js
    Update

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from './(components)/Navbar';
 import Footer from './(components)/Footer';
+// IMPORT BARU
+import { LanguageProvider } from './context/LanguageContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,11 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
+      {/* WRAP DENGAN LANGUAGE PROVIDER */}
+      <LanguageProvider>
+        <body className={inter.className}>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </LanguageProvider>
     </html>
   );
 }
